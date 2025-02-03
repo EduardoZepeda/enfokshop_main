c7cba7c50d0d57fb5dc979cd80bbd39d08152c57
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2025-02-02 23:44:44 -0600

    Add credits and min css

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
         <div className='intro'>
           <h1 className="main-title">Enfok shop</h1>
           <h2 className="main-subtitle">Tu tienda online de accesorios para fotografía y video</h2>
+          <button><a href="https://facebook.com/enfokshop">Contacto</a></button>
         </div>
+        <div className="photo-credits"><a className="credits" rel="nofollow noopener" href="https://www.pexels.com/@cottonbro/">Picture Credits: cottonbro</a></div>
       </main>
     </>
   )
