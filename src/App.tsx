6891467cb61e962cc274122f27f1617ebf124c75
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2025-02-02 22:49:20 -0600

    Initial state, basic frontpage

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='main geist-font'>
+        <div className='intro'>
+          <h1 className="main-title">Enfok shop</h1>
+          <h2 className="main-subtitle">Tu tienda online de accesorios para fotografía y video</h2>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
